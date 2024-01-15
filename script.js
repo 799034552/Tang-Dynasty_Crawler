363b4e5c49270846182986012d3b4ad8c418b613
string <799034552@qq.com>
2024-01-15 17:34:43 +0800

    initialize

---
+++ script.js
@@ -0,0 +1,197 @@
+var _0x3e9e = ["c3BsaXQ=", "c2xpY2U=", "dG9TdHJpbmc=", "c2V0VGltZQ==", "Z2V0VGltZQ==", "Y29va2ll", "YWN3X3NjX192Mj0=", "O2V4cGlyZXM9", "dG9HTVRTdHJpbmc=", "O21heC1hZ2U9MzYwMDtwYXRoPS8=", "MzAwMDE3NjAwMDg1NjAwNjA2MTUwMTUzMzAwMzY5MDAyNzgwMDM3NQ==", "bGVuZ3Ro", "am9pbg==", "MXw0fDN8MHwy"];
+(function(_0x2d8f05, _0x4b81bb) {
+    var _0x4d74cb = function(_0x32719f) {
+        while (--_0x32719f) {
+            _0x2d8f05["push"](_0x2d8f05["shift"]())
+        }
+    };
+    var _0x33748d = function() {
+        var _0x3e4c21 = {
+            "data": {
+                "key": "cookie",
+                "value": "timeout"
+            },
+            "setCookie": function(_0x5c685e, _0x3e3156, _0x1e9e81, _0x292610) {
+                _0x292610 = _0x292610 || {};
+                var _0x151bd2 = _0x3e3156 + "=" + _0x1e9e81;
+                var _0x558098 = 0;
+                for (var _0x558098 = 0, _0x230f38 = _0x5c685e["length"]; _0x558098 < _0x230f38; _0x558098++) {
+                    var _0x948b6c = _0x5c685e[_0x558098];
+                    _0x151bd2 += ";\x20" + _0x948b6c;
+                    var _0x29929c = _0x5c685e[_0x948b6c];
+                    _0x5c685e["push"](_0x29929c);
+                    _0x230f38 = _0x5c685e["length"];
+                    if (_0x29929c !== !![]) {
+                        _0x151bd2 += "=" + _0x29929c
+                    }
+                }
+                _0x292610["cookie"] = _0x151bd2
+            },
+            "removeCookie": function() {
+                return "dev"
+            },
+            "getCookie": function(_0x5dd881, _0x550fbc) {
+                _0x5dd881 = _0x5dd881 || function(_0x18d5c9) {
+                    return _0x18d5c9
+                }
+                ;
+                var _0x4ce2f1 = _0x5dd881(new RegExp("(?:^|;\x20)" + _0x550fbc["replace"](/([.$?*|{}()[]\/+^])/g, "$1") + "=([^;]*)"));
+                var _0x333808 = function(_0x432180, _0x2ab90b) {
+                    _0x432180(++_0x2ab90b)
+                };
+                _0x333808(_0x4d74cb, _0x4b81bb);
+                return _0x4ce2f1 ? decodeURIComponent(_0x4ce2f1[1]) : undefined
+            }
+        };
+        var _0x991246 = function() {
+            var _0x981158 = new RegExp("\x5cw+\x20*\x5c(\x5c)\x20*{\x5cw+\x20*[\x27|\x22].+[\x27|\x22];?\x20*}");
+            return true //_0x981158["test"](_0x3e4c21["removeCookie"]["toString"]())
+        };
+        _0x3e4c21["updateCookie"] = _0x991246;
+        var _0x57b080 = "";
+        var _0x219af0 = _0x3e4c21["updateCookie"]();
+        if (!_0x219af0) {
+            _0x3e4c21["setCookie"](["*"], "counter", 1)
+        } else {
+            if (_0x219af0) {
+                _0x57b080 = _0x3e4c21["getCookie"](null, "counter")
+            } else {
+                _0x3e4c21["removeCookie"]()
+            }
+        }
+    };
+    _0x33748d()
+}(_0x3e9e, 374));
+var _0x1e8e = function(_0x558645, _0x3571ed) {
+    _0x558645 = _0x558645 - 0;
+    var _0x23d32b = _0x3e9e[_0x558645];
+    if (_0x1e8e["jweSQB"] === undefined) {
+        (function() {
+            var _0x2a4aae;
+            try {
+                var _0x1ac753 = Function("return\x20(function()\x20" + "{}.constructor(\x22return\x20this\x22)(\x20)" + ");");
+                _0x2a4aae = _0x1ac753()
+            } catch (_0x267ba9) {
+                _0x2a4aae = window
+            }
+            var _0x22c6cf = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+            _0x2a4aae["atob"] || (_0x2a4aae["atob"] = function(_0xb01b66) {
+                var _0x112e38 = String(_0xb01b66)["replace"](/=+$/, "");
+                for (var _0x315811 = 0, _0x196945, _0x8ee65b, _0x111e6b = 0, _0x2a5e7f = ""; _0x8ee65b = _0x112e38["charAt"](_0x111e6b++); ~_0x8ee65b && (_0x196945 = _0x315811 % 4 ? _0x196945 * 64 + _0x8ee65b : _0x8ee65b,
+                _0x315811++ % 4) ? _0x2a5e7f += String["fromCharCode"](255 & _0x196945 >> (-2 * _0x315811 & 6)) : 0) {
+                    _0x8ee65b = _0x22c6cf["indexOf"](_0x8ee65b)
+                }
+                return _0x2a5e7f
+            }
+            )
+        }());
+        _0x1e8e["VidPVs"] = function(_0x539abf) {
+            var _0x126fa5 = atob(_0x539abf);
+            var _0x54d768 = [];
+            for (var _0x3d3645 = 0, _0x4289fc = _0x126fa5["length"]; _0x3d3645 < _0x4289fc; _0x3d3645++) {
+                _0x54d768 += "%" + ("00" + _0x126fa5["charCodeAt"](_0x3d3645)["toString"](16))["slice"](-2)
+            }
+            return decodeURIComponent(_0x54d768)
+        }
+        ;
+        _0x1e8e["BXvRsu"] = {};
+        _0x1e8e["jweSQB"] = !![]
+    }
+    var _0x436197 = _0x1e8e["BXvRsu"][_0x558645];
+    if (_0x436197 === undefined) {
+        var _0x4f4121 = function(_0x5e2adc) {
+            this["nlcXFw"] = _0x5e2adc;
+            this["HAmvBE"] = [1, 0, 0];
+            this["YFWLey"] = function() {
+                return "newState"
+            }
+            ;
+            this["YpNXEl"] = "\x5cw+\x20*\x5c(\x5c)\x20*{\x5cw+\x20*";
+            this["JsKhOp"] = "[\x27|\x22].+[\x27|\x22];?\x20*}"
+        };
+        _0x4f4121["prototype"]["pzRiIQ"] = function() {
+            var _0x3e581e = new RegExp(this["YpNXEl"] + this["JsKhOp"]);
+            var _0x13a005 = _0x3e581e["test"](this["YFWLey"]["toString"]()) ? --this["HAmvBE"][1] : --this["HAmvBE"][0];
+            return true//this["gaiPha"](_0x13a005)
+        }
+        ;
+        _0x4f4121["prototype"]["gaiPha"] = function(_0x1e6387) {
+            if (!Boolean(~_0x1e6387)) {
+                return _0x1e6387
+            }
+            return this["hpKQFb"](this["nlcXFw"])
+        }
+        ;
+        _0x4f4121["prototype"]["hpKQFb"] = function(_0x20dc19) {
+            for (var _0x19d402 = 0, _0x5a3818 = this["HAmvBE"]["length"]; _0x19d402 < _0x5a3818; _0x19d402++) {
+                this["HAmvBE"]["push"](Math["round"](Math["random"]()));
+                _0x5a3818 = this["HAmvBE"]["length"]
+            }
+            return _0x20dc19(this["HAmvBE"][0])
+        }
+        ;
+        new _0x4f4121(_0x1e8e)["pzRiIQ"]();
+        _0x23d32b = _0x1e8e["VidPVs"](_0x23d32b);
+        _0x1e8e["BXvRsu"][_0x558645] = _0x23d32b
+    } else {
+        _0x23d32b = _0x436197
+    }
+    return _0x23d32b
+};
+
+var get_cookie = (arg1)=> {
+    var posList = [15, 35, 29, 24, 33, 16, 1, 38, 10, 9, 19, 31, 40, 27, 22, 23, 25, 13, 6, 11, 39, 18, 20, 8, 14, 21, 32, 26, 2, 30, 7, 4, 17, 5, 3, 28, 34, 37, 12, 36];
+    var mask = _0x1e8e("0x0");
+    var outPutList = [];
+    var arg2 = "";
+    var arg3 = "";
+    for (var i = 0; i < arg1[_0x1e8e("0x1")]; i++) {
+        var this_i = arg1[i];
+        for (var j = 0; j < posList[_0x1e8e("0x1")]; j++) {
+            if (posList[j] == i + 1) {
+                outPutList[j] = this_i
+            }
+        }
+    }
+    arg2 = outPutList[_0x1e8e("0x2")]("");
+    for (var i = 0; i < arg2[_0x1e8e("0x1")] && i < mask[_0x1e8e("0x1")]; i += 2) {
+        var GxjQsM = _0x1e8e("0x3")[_0x1e8e("0x4")]("|")
+            , QoWazb = 0;
+        while (!![]) {
+            switch (GxjQsM[QoWazb++]) {
+            case "0":
+                if (xorChar[_0x1e8e("0x1")] == 1) {
+                    xorChar = "0" + xorChar
+                }
+                continue;
+            case "1":
+                var strChar = parseInt(arg2[_0x1e8e("0x5")](i, i + 2), 16);
+                continue;
+            case "2":
+                arg3 += xorChar;
+                continue;
+            case "3":
+                var xorChar = (strChar ^ maskChar)[_0x1e8e("0x6")](16);
+                continue;
+            case "4":
+                var maskChar = parseInt(mask[_0x1e8e("0x5")](i, i + 2), 16);
+                continue
+            }
+            break
+        }
+    }
+    var expiredate = new Date();
+    expiredate[_0x1e8e("0x7")](expiredate[_0x1e8e("0x8")]() + 3600 * 1000);
+    var theHost = "cdt-ec.com"
+        , theHostSplit = theHost.split(".")
+        , theHostSplitLength = theHostSplit.length;
+    !/^(\d+\.)*\d+$/.test(theHost) && theHostSplitLength > 2 && ("com.cn" != (theHost = theHostSplit[theHostSplitLength - 2] + "." + theHostSplit[theHostSplitLength - 1]) && "gov.cn" != theHost && "org.cn" != theHost && "net.cn" != theHost && "com.my" != theHost || (theHost = theHostSplit[theHostSplitLength - 3] + "." + theHost));
+    res_cookie = _0x1e8e("0xa") + arg3 + _0x1e8e("0xb") + expiredate[_0x1e8e("0xc")]() + _0x1e8e("0xd") + ";domain=" + theHost
+    res_cookie = res_cookie.split("=")[1].split(";")[0]
+    // console.log(res_cookie)
+    return res_cookie
+}
+// get_cookie("0512354E5A88435C43B7C764100CAD04CD5EC2ED")
+
+console.log(get_cookie("1991E7CC2E2E9A59C256399477D4A9EEC651BFB9"))
+// arg1 = "0512354E5A88435C43B7C764100CAD04CD5EC2ED"
---
+++ 佛山.py
@@ -0,0 +1,468 @@
+js_script = r"""
+var _0x3e9e = ["c3BsaXQ=", "c2xpY2U=", "dG9TdHJpbmc=", "c2V0VGltZQ==", "Z2V0VGltZQ==", "Y29va2ll", "YWN3X3NjX192Mj0=", "O2V4cGlyZXM9", "dG9HTVRTdHJpbmc=", "O21heC1hZ2U9MzYwMDtwYXRoPS8=", "MzAwMDE3NjAwMDg1NjAwNjA2MTUwMTUzMzAwMzY5MDAyNzgwMDM3NQ==", "bGVuZ3Ro", "am9pbg==", "MXw0fDN8MHwy"];
+(function(_0x2d8f05, _0x4b81bb) {
+    var _0x4d74cb = function(_0x32719f) {
+        while (--_0x32719f) {
+            _0x2d8f05["push"](_0x2d8f05["shift"]())
+        }
+    };
+    var _0x33748d = function() {
+        var _0x3e4c21 = {
+            "data": {
+                "key": "cookie",
+                "value": "timeout"
+            },
+            "setCookie": function(_0x5c685e, _0x3e3156, _0x1e9e81, _0x292610) {
+                _0x292610 = _0x292610 || {};
+                var _0x151bd2 = _0x3e3156 + "=" + _0x1e9e81;
+                var _0x558098 = 0;
+                for (var _0x558098 = 0, _0x230f38 = _0x5c685e["length"]; _0x558098 < _0x230f38; _0x558098++) {
+                    var _0x948b6c = _0x5c685e[_0x558098];
+                    _0x151bd2 += ";\x20" + _0x948b6c;
+                    var _0x29929c = _0x5c685e[_0x948b6c];
+                    _0x5c685e["push"](_0x29929c);
+                    _0x230f38 = _0x5c685e["length"];
+                    if (_0x29929c !== !![]) {
+                        _0x151bd2 += "=" + _0x29929c
+                    }
+                }
+                _0x292610["cookie"] = _0x151bd2
+            },
+            "removeCookie": function() {
+                return "dev"
+            },
+            "getCookie": function(_0x5dd881, _0x550fbc) {
+                _0x5dd881 = _0x5dd881 || function(_0x18d5c9) {
+                    return _0x18d5c9
+                }
+                ;
+                var _0x4ce2f1 = _0x5dd881(new RegExp("(?:^|;\x20)" + _0x550fbc["replace"](/([.$?*|{}()[]\/+^])/g, "$1") + "=([^;]*)"));
+                var _0x333808 = function(_0x432180, _0x2ab90b) {
+                    _0x432180(++_0x2ab90b)
+                };
+                _0x333808(_0x4d74cb, _0x4b81bb);
+                return _0x4ce2f1 ? decodeURIComponent(_0x4ce2f1[1]) : undefined
+            }
+        };
+        var _0x991246 = function() {
+            var _0x981158 = new RegExp("\x5cw+\x20*\x5c(\x5c)\x20*{\x5cw+\x20*[\x27|\x22].+[\x27|\x22];?\x20*}");
+            return true //_0x981158["test"](_0x3e4c21["removeCookie"]["toString"]())
+        };
+        _0x3e4c21["updateCookie"] = _0x991246;
+        var _0x57b080 = "";
+        var _0x219af0 = _0x3e4c21["updateCookie"]();
+        if (!_0x219af0) {
+            _0x3e4c21["setCookie"](["*"], "counter", 1)
+        } else {
+            if (_0x219af0) {
+                _0x57b080 = _0x3e4c21["getCookie"](null, "counter")
+            } else {
+                _0x3e4c21["removeCookie"]()
+            }
+        }
+    };
+    _0x33748d()
+}(_0x3e9e, 374));
+var _0x1e8e = function(_0x558645, _0x3571ed) {
+    _0x558645 = _0x558645 - 0;
+    var _0x23d32b = _0x3e9e[_0x558645];
+    if (_0x1e8e["jweSQB"] === undefined) {
+        (function() {
+            var _0x2a4aae;
+            try {
+                var _0x1ac753 = Function("return\x20(function()\x20" + "{}.constructor(\x22return\x20this\x22)(\x20)" + ");");
+                _0x2a4aae = _0x1ac753()
+            } catch (_0x267ba9) {
+                _0x2a4aae = window
+            }
+            var _0x22c6cf = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+            _0x2a4aae["atob"] || (_0x2a4aae["atob"] = function(_0xb01b66) {
+                var _0x112e38 = String(_0xb01b66)["replace"](/=+$/, "");
+                for (var _0x315811 = 0, _0x196945, _0x8ee65b, _0x111e6b = 0, _0x2a5e7f = ""; _0x8ee65b = _0x112e38["charAt"](_0x111e6b++); ~_0x8ee65b && (_0x196945 = _0x315811 % 4 ? _0x196945 * 64 + _0x8ee65b : _0x8ee65b,
+                _0x315811++ % 4) ? _0x2a5e7f += String["fromCharCode"](255 & _0x196945 >> (-2 * _0x315811 & 6)) : 0) {
+                    _0x8ee65b = _0x22c6cf["indexOf"](_0x8ee65b)
+                }
+                return _0x2a5e7f
+            }
+            )
+        }());
+        _0x1e8e["VidPVs"] = function(_0x539abf) {
+            var _0x126fa5 = atob(_0x539abf);
+            var _0x54d768 = [];
+            for (var _0x3d3645 = 0, _0x4289fc = _0x126fa5["length"]; _0x3d3645 < _0x4289fc; _0x3d3645++) {
+                _0x54d768 += "%" + ("00" + _0x126fa5["charCodeAt"](_0x3d3645)["toString"](16))["slice"](-2)
+            }
+            return decodeURIComponent(_0x54d768)
+        }
+        ;
+        _0x1e8e["BXvRsu"] = {};
+        _0x1e8e["jweSQB"] = !![]
+    }
+    var _0x436197 = _0x1e8e["BXvRsu"][_0x558645];
+    if (_0x436197 === undefined) {
+        var _0x4f4121 = function(_0x5e2adc) {
+            this["nlcXFw"] = _0x5e2adc;
+            this["HAmvBE"] = [1, 0, 0];
+            this["YFWLey"] = function() {
+                return "newState"
+            }
+            ;
+            this["YpNXEl"] = "\x5cw+\x20*\x5c(\x5c)\x20*{\x5cw+\x20*";
+            this["JsKhOp"] = "[\x27|\x22].+[\x27|\x22];?\x20*}"
+        };
+        _0x4f4121["prototype"]["pzRiIQ"] = function() {
+            var _0x3e581e = new RegExp(this["YpNXEl"] + this["JsKhOp"]);
+            var _0x13a005 = _0x3e581e["test"](this["YFWLey"]["toString"]()) ? --this["HAmvBE"][1] : --this["HAmvBE"][0];
+            return true//this["gaiPha"](_0x13a005)
+        }
+        ;
+        _0x4f4121["prototype"]["gaiPha"] = function(_0x1e6387) {
+            if (!Boolean(~_0x1e6387)) {
+                return _0x1e6387
+            }
+            return this["hpKQFb"](this["nlcXFw"])
+        }
+        ;
+        _0x4f4121["prototype"]["hpKQFb"] = function(_0x20dc19) {
+            for (var _0x19d402 = 0, _0x5a3818 = this["HAmvBE"]["length"]; _0x19d402 < _0x5a3818; _0x19d402++) {
+                this["HAmvBE"]["push"](Math["round"](Math["random"]()));
+                _0x5a3818 = this["HAmvBE"]["length"]
+            }
+            return _0x20dc19(this["HAmvBE"][0])
+        }
+        ;
+        new _0x4f4121(_0x1e8e)["pzRiIQ"]();
+        _0x23d32b = _0x1e8e["VidPVs"](_0x23d32b);
+        _0x1e8e["BXvRsu"][_0x558645] = _0x23d32b
+    } else {
+        _0x23d32b = _0x436197
+    }
+    return _0x23d32b
+};
+
+var get_cookie = (arg1)=> {
+    var posList = [15, 35, 29, 24, 33, 16, 1, 38, 10, 9, 19, 31, 40, 27, 22, 23, 25, 13, 6, 11, 39, 18, 20, 8, 14, 21, 32, 26, 2, 30, 7, 4, 17, 5, 3, 28, 34, 37, 12, 36];
+    var mask = _0x1e8e("0x0");
+    var outPutList = [];
+    var arg2 = "";
+    var arg3 = "";
+    for (var i = 0; i < arg1[_0x1e8e("0x1")]; i++) {
+        var this_i = arg1[i];
+        for (var j = 0; j < posList[_0x1e8e("0x1")]; j++) {
+            if (posList[j] == i + 1) {
+                outPutList[j] = this_i
+            }
+        }
+    }
+    arg2 = outPutList[_0x1e8e("0x2")]("");
+    for (var i = 0; i < arg2[_0x1e8e("0x1")] && i < mask[_0x1e8e("0x1")]; i += 2) {
+        var GxjQsM = _0x1e8e("0x3")[_0x1e8e("0x4")]("|")
+            , QoWazb = 0;
+        while (!![]) {
+            switch (GxjQsM[QoWazb++]) {
+            case "0":
+                if (xorChar[_0x1e8e("0x1")] == 1) {
+                    xorChar = "0" + xorChar
+                }
+                continue;
+            case "1":
+                var strChar = parseInt(arg2[_0x1e8e("0x5")](i, i + 2), 16);
+                continue;
+            case "2":
+                arg3 += xorChar;
+                continue;
+            case "3":
+                var xorChar = (strChar ^ maskChar)[_0x1e8e("0x6")](16);
+                continue;
+            case "4":
+                var maskChar = parseInt(mask[_0x1e8e("0x5")](i, i + 2), 16);
+                continue
+            }
+            break
+        }
+    }
+    var expiredate = new Date();
+    expiredate[_0x1e8e("0x7")](expiredate[_0x1e8e("0x8")]() + 3600 * 1000);
+    var theHost = "cdt-ec.com"
+        , theHostSplit = theHost.split(".")
+        , theHostSplitLength = theHostSplit.length;
+    !/^(\d+\.)*\d+$/.test(theHost) && theHostSplitLength > 2 && ("com.cn" != (theHost = theHostSplit[theHostSplitLength - 2] + "." + theHostSplit[theHostSplitLength - 1]) && "gov.cn" != theHost && "org.cn" != theHost && "net.cn" != theHost && "com.my" != theHost || (theHost = theHostSplit[theHostSplitLength - 3] + "." + theHost));
+    res_cookie = _0x1e8e("0xa") + arg3 + _0x1e8e("0xb") + expiredate[_0x1e8e("0xc")]() + _0x1e8e("0xd") + ";domain=" + theHost
+    res_cookie = res_cookie.split("=")[1].split(";")[0]
+    // console.log(res_cookie)
+    return res_cookie
+}
+
+"""
+
+
+import requests
+from datetime import datetime,timedelta
+import schedule
+import time
+import os
+import execjs
+
+# 获取关键字
+script_path = os.path.abspath(__file__)
+script_name = os.path.basename(script_path).split(".")[0]
+print(f"关键词为：{script_name}")
+
+
+# 保存字符串到文本文件
+def save_to_file(data, file_path="./cookie.txt"):
+    with open(file_path, "w") as file:
+        file.write(data)
+
+# 从文本文件加载字符串
+def load_from_file(file_path="./cookie.txt"):
+    loaded_data = ""
+    try:
+        with open(file_path, "r") as file:
+            loaded_data = file.read()
+        print("从文件加载的字符串:", loaded_data)
+    except Exception as e:
+        pass
+    return loaded_data
+
+
+cookie = load_from_file()
+# cookie = ""
+
+def get_arg1(txt):
+    data = txt
+    arg1 = data.split("var arg1=\'")[1].split("\'")[0]
+    return arg1
+
+def get_cookie(txt=None):
+    print("过期，重新登录了")
+    if txt is None:
+        # 创建一个会话对象
+        session = requests.Session()
+        # 设置用户代理
+        session.headers.update({
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+            "Origin": "https://tang.cdt-ec.com",
+            "Referer": "https://tang.cdt-ec.com/notice/webpage/jsp/more.jsp",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+            "X-Requested-With": "XMLHttpRequest",
+            "sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Windows"'
+        })
+
+        # 添加Cookie
+        
+        # session.cookies.set('acw_sc__v2', cookie, domain='.cdt-ec.com', path='/')
+
+        # 设置POST请求的数据
+        data = {
+            "page": "1",
+            "limit": "10",
+            "messagetype": "0",
+            "startDate": "",
+            "endDate": "",
+        }
+        
+        # 发送POST请求
+        response = session.post(
+            "https://tang.cdt-ec.com/notice/moreController/getList",
+            data=data
+        )
+        txt = response.text
+    try:
+        arg1 = get_arg1(txt)
+        js_code = js_script 
+        ctx = execjs.compile(js_code)
+        result = ctx.call("get_cookie", arg1)  # 调用JavaScript函数
+    except:
+        print("无法访问了\n")
+        exit()
+    return result
+        
+
+
+def get_new_item(keyword="佛山", deep=0):
+    global cookie
+    if (cookie == ""):
+        cookie = get_cookie()
+        save_to_file(cookie)
+    # 创建一个会话对象
+    session = requests.Session()
+    # 设置用户代理
+    session.headers.update({
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+        "Origin": "https://tang.cdt-ec.com",
+        "Referer": "https://tang.cdt-ec.com/notice/webpage/jsp/more.jsp",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        # "X-Forwarded-For": "123.456.789.45",
+        "Sec-Fetch-Site": "same-origin",
+        "X-Requested-With": "XMLHttpRequest",
+        "sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"'
+    })
+
+    # 添加Cookie
+    
+    session.cookies.set('acw_sc__v2', cookie, domain='.cdt-ec.com', path='/')
+
+    # 设置POST请求的数据
+    data = {
+        "page": "1",
+        "limit": "10",
+        "messagetype": "0",
+        "pro_bidding_mothod": "0",
+        "message_title": "",
+        "purchase_unit": f"{keyword}",
+        "purchase_unit_agent": "",
+        "message_no": "",
+        "purchase_code": "",
+        "startDate": "",
+        "endDate": "",
+        "purchase_type": "0"
+    }
+    try:
+        # 发送POST请求
+        response = session.post(
+            "https://tang.cdt-ec.com/notice/moreController/getList",
+            data=data
+        )
+        # print(response.text)
+        res = response.json()
+        if len(res["data"]) == 0:
+            return None
+        res = res["data"][0]
+    except:
+        if (deep == 0):
+            cookie = get_cookie(response.text)
+            save_to_file(cookie)
+            return get_new_item(keyword, deep+1)
+        else:
+            print("可能被封ip了")
+            exit()
+            res = None
+    return res
+
+
+def check_and_add_string(input_str, file_name="./已发送过的信息.txt"):
+    try:
+        # 尝试打开文件以读取内容
+        with open(file_name, 'r') as file:
+            content = file.read()
+            
+            # 检查字符串是否在文件内容中
+            if input_str in content:
+                return False  # 字符串已存在，返回False
+    except FileNotFoundError:
+        pass  # 文件不存在，继续后续操作
+    
+    # 字符串不存在于文件中，将其添加到文件
+    with open(file_name, 'a') as file:
+        file.write(input_str + '\n')
+    
+    return True  # 字符串添加到文件，返回True
+
+
+
+
+def get_target_res(keyword="佛山"):
+    # 获取最新的项
+    new_item = get_new_item(keyword)
+    if new_item is None:
+        return None, False
+    # 判断时间与现在是否相差2天以内
+    new_time = new_item["publish_time"]
+    new_title = new_item["message_title"]
+    new_time_obj = datetime.strptime(new_time, '%Y-%m-%d %H:%M:%S')
+    #获取当前时间
+    current_date = datetime.now()
+    difference = current_date - new_time_obj
+    
+    if difference <= timedelta(days=2):
+        return new_item, True
+    else:
+        return new_item, False
+    
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
+
+def send_email(sender_email, passwd,recipient_email, subject, message):
+    my_sender = sender_email 
+    my_pass = passwd 
+    my_user = recipient_email  # 收件人邮箱账号
+    ret = True
+    try:
+        msg = MIMEText(message, 'plain', 'utf-8')  # 填写邮件内容
+        msg['From'] = formataddr(["【自动通知】", my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+        msg['To'] = formataddr(["", my_user])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+        msg['Subject'] = subject  # 邮件的主题，也可以说是标题
+
+        server = smtplib.SMTP_SSL("smtp.126.com", 465)  # 发件人邮箱中的SMTP服务器
+        server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱授权码
+        server.sendmail(my_sender, [my_user, ], msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+        server.quit()  # 关闭连接
+    except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+        ret = False
+    return ret
+    
+
+def your_task(keywork="佛山"):
+    new_item, status = get_target_res(keywork)
+    now_time = datetime.now().strftime("%Y-%m-%d %H:%M")
+    if new_item is None:
+        print(f"[{now_time}] 无数据")
+        return
+    new_time = new_item["publish_time"]
+    new_title = new_item["message_title"]
+    print(f"[{now_time}] 最新的数据是：{new_title}，{new_time}")
+    item_id = new_title + " " +new_time
+    if status: # 相差两天以内
+        if check_and_add_string(item_id): # 如果没有发送过
+            send_status = send_email("xxxxxxx@126.com", "授权码", "xxxxx@qq.com", "有最近两天内的招标",f"{item_id}")
+            if (send_status):
+                print(f"[{now_time}] 邮件发送成功")
+            else:
+                print(f"[{now_time}] 邮件发送失败")
+        else:
+            print(f"[{now_time}] 已经发送过了")
+    else:
+        print(f"[{now_time}] 最新记录距离今天相差超过两天")
+
+def no_arg_task():
+    your_task(script_name)
+
+# 每天早上8点执行
+schedule.every().day.at("08:00").do(no_arg_task)
+# # 每天早上8点执行
+# schedule.every().day.at("12:00").do(no_arg_task)
+# # 每天晚上8点执行
+# schedule.every().day.at("20:00").do(no_arg_task)
+
+# 先运行一次
+no_arg_task()
+
+def spinning_cursor():
+    while True:
+        for cursor in '|/-\\':
+            yield cursor
+
+spinner = spinning_cursor()
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+    print("正在运行中:"+next(spinner), end="\r")
+    
+
+    
+
